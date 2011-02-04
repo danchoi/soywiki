ab6227227b3cc563570ad0f50e60d81066513012
Daniel Choi <dhchoi@gmail.com>
2011-02-04 09:45:25 -0500

    Save page works across drb connection

--- lib/zenwiki/server.rb
+++ lib/zenwiki/server.rb
@@ -1,9 +1,21 @@
 require 'drb'
+require 'logger'
 
 class Zenwiki
   class Server
 
-    def initialize
+    def initialize(config={'logfile' => 'zenwiki.log'})
+      @logger = Logger.new(config['logfile'] || STDERR)
+      @logger.level = Logger::DEBUG 
+    end
+
+    def save_page(page)
+      log "Saving page"
+      log page
+    end
+
+    def log(text)
+      @logger.debug text
     end
 
     def self.start
